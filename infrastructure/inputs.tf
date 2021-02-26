dace7f426f7eba9c08bd744b3cc3b4c68242770d
Johannes Ebner <Structed@users.noreply.github.com>
2021-02-26 21:37:02 +0100

    Initial terraform commit
    creates:
    - resource group
    - vnet
    - subnet in vnet

---
+++ infrastructure/inputs.tf
@@ -0,0 +1,22 @@
+﻿variable "rgName" {
+  type = string
+  description = "The resource group name"
+  default = "rg-gamezure"
+}
+
+variable "location" {
+  type = string
+  description = "The location in which to create all resources"
+  default = "westeurope"
+}
+
+variable "prefix" {
+  type = string
+  description = "Resource Name prefix (will be applied to all resource names except the resource group"
+  default = "gamezure"
+}
+
+variable "tags" {
+  type = list(string)
+  default = []
+}
