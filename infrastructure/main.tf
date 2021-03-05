179d538d589bc792d0831d39e66950ef42d6201a
Johannes Ebner <Structed@users.noreply.github.com>
2021-03-06 00:03:01 +0100

    Added storage account and function app

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -49,8 +49,60 @@ resource "azurerm_subnet" "subnet_vmpool" {
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+resource "azurerm_storage_account" "storage" {
+  name                     = replace("${var.prefix}storage", "-", "")
+  resource_group_name      = azurerm_resource_group.rg_management.name
+  location                 = azurerm_resource_group.rg_management.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  access_tier              = "Hot"
+}
+
 module "cosmosdb" {
   source         = "./cosmosdb"
   resource_group = azurerm_resource_group.rg_management
   tags           = var.tags
 }
+
+
+resource "azurerm_app_service_plan" "app_service_plan" {
+  name                = "${var.prefix}-app-service-plan"
+  location            = azurerm_resource_group.rg_management.location
+  resource_group_name = azurerm_resource_group.rg_management.name
+  kind                = "FunctionApp"
+
+  sku {
+    tier = "Dynamic"
+    size = "Y1"
+  }
+}
+
+resource "azurerm_function_app" "function" {
+  name                       = "${var.prefix}-function"
+  location                   = azurerm_resource_group.rg_management.location
+  resource_group_name        = azurerm_resource_group.rg_management.name
+  app_service_plan_id        = azurerm_app_service_plan.app_service_plan.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
+
+  enabled                = true
+  enable_builtin_logging = true
+  https_only             = true
+//  identity {
+//    type = ""
+//  }
+
+  connection_string {
+    name  = "CosmosDb"
+    type  = "DocDb"
+    value = module.cosmosdb.connection_strings[0]
+  }
+
+  site_config {
+    ftps_state = "Disabled"
+    //    ip_restriction = []
+    min_tls_version = "1.2"
+  }
+}
+
