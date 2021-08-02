3fdfcbeaed9836b00628326db353044c09fc1c91
Johannes Ebner <Structed@users.noreply.github.com>
2021-08-02 08:53:41 +0200

    Added Startup.cs and DependencyInjection packages

---
+++ application/Gamezure.VmPoolManager/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Gamezure.VmPoolManager.Repository;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Gamezure.VmPoolManager.Startup))]
+
+namespace Gamezure.VmPoolManager
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // builder.Services.AddHttpClient();
+
+            string connectionString = Environment.GetEnvironmentVariable("CosmosDb");
+            builder.Services.AddSingleton<PoolRepository>((s) => {
+                return new PoolRepository(connectionString);
+            });
+        }
+    }
+}
